0554dcec3611702a71b6cfbf894ef17eaabd2f92
carlsjones65 <cjones2@saintjoe.edu>
2015-04-29 22:27:10 -0400

    Converted to the phone homework because i couldn't make and merge the java and git repos together

--- Right Now/src/edu/saintjoe/cs/carlj/RightNow/RightNow.java
+++ Right Now/src/edu/saintjoe/cs/carlj/RightNow/RightNow.java
@@ -2,5 +2,36 @@ package edu.saintjoe.cs.carlj.RightNow;
 
 public class RightNow {
 	//this is just a pretend project
-
+	
+	public final String name;
+	
+	public Object year;
+	
+	public RightNow(String name) {
+		this.name = name;
+	}
+	
+	public RightNow (String name, Object year) {
+		this.name = name;
+		this.year = year;
+		
 }
+	
+	public String getname() {
+		return name;
+	}
+	
+	public Object getyear() {
+		return year;
+	}
+	
+	public String toString() {
+		return name + "='" + year + "'";
+	}
+}
+	
+
+	
+	
+	
+	
